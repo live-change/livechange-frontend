1c14e80d86009bb0d018fb55ffff63051b3b3346
Michał Łaszczewski <michal@laszczewski.com>
2022-08-22 07:03:48 +0200

    licenses

--- access-control-frontend/e2e/inviteExistingUser.test.js
+++ access-control-frontend/e2e/inviteExistingUser.test.js
@@ -5,12 +5,18 @@ Feature('access control invite')
 Scenario('invite user that already exists', async ({ I }) => {
 
   const adminUser = await I.haveUser()
-  const invitedUser = await I.haveUser()
+  const anotherUser = await I.haveUser()
   await I.haveUserWithAccess(adminUser, 'example_Example', 'one', ['administrator'])
 
   await I.amOnPage('/')
   await I.amLoggedIn(adminUser)
 
-  I.assert('it will fail', false)
+  I.see('Access Granted')
+  I.click('Share')
+  I.see('Access Control')
+  I.click('Invite with email')
+  I.see('Invite user with email')
+  I.fillField('input[type="text"]', anotherUser.email)
+  I.click('Invite')
 
 })
