59293bdce28cf870631440e0c65fb8a7f0929b89
Michał Łaszczewski <michal@laszczewski.com>
2022-08-31 14:29:07 +0200

    pages-frontend

---
+++ pages-frontend/index.js
@@ -0,0 +1,7 @@
+import ImageUpload from "./front/src/ImageUpload.js"
+import ImageEditor from "./front/src/ImageEditor.vue"
+import Image from "./front/src/Image.vue"
+import {uploadImage, imageUploads} from "./front/src/imageUploads.js";
+import preProcessImageFile from "./front/src/preprocessImageFile.js";
+
+export { ImageUpload, Image, uploadImage, preProcessImageFile, imageUploads, ImageEditor }
