0363aa6413641cf675a8f382788be4064c5e930b
Marzenad42 <54839173+Marzenad42@users.noreply.github.com>
2022-09-08 10:47:27 +0200

    invite-existing-user-test

--- access-control-frontend/e2e/inviteExistingUser.test.js
+++ access-control-frontend/e2e/inviteExistingUser.test.js
@@ -11,12 +11,13 @@ Scenario('invite user that already exists', async ({ I }) => {
   await I.amOnPage('/')
   await I.amLoggedIn(adminUser)
 
-  I.see('Access Granted')
+  I.see('Access Granted!')
   I.click('Share')
   I.see('Access Control')
   I.click('Invite with email')
   I.see('Invite user with email')
-  I.fillField('input[type="text"]', anotherUser.email)
+  I.see('Email address')
+  I.fillField('input[id="email"]', anotherUser.email)
   I.click('Invite')
-
+  I.click('Invite')
 })
