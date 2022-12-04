dde8bbcc239f8a3e91136c478e8063af6f977a4c
Michał Łaszczewski <michal@laszczewski.com>
2022-12-04 18:21:41 +0100

    export content functions and components

--- content-frontend/index.js
+++ content-frontend/index.js
@@ -1,3 +1,18 @@
+import Content from './front/src/components/Content.vue'
+import ContentEditor from './front/src/components/ContentEditor.vue'
+import Metadata from './front/src/components/Metadata.vue'
+import MetadataEditor from './front/src/components/MetadataEditor.vue'
+import NotFoundAdminButtons from './front/src/components/NotFoundAdminButtons.vue'
+import Page from './front/src/components/Page.vue'
+import PageEditor from './front/src/components/PageEditor.vue'
+import PagePreview from './front/src/components/PagePreview.vue'
+import PageAdminButtons from './front/src/components/PageAdminButtons.vue'
 
+export {
+  Content, ContentEditor, Metadata, MetadataEditor, NotFoundAdminButtons, Page, PageEditor, PagePreview,
+  PageAdminButtons
+}
 
-export { }
+import { catchAllPagesRoute, contentEditRoutes, pagesSitemap } from "./front/src/components/routes"
+
+export { catchAllPagesRoute, contentEditRoutes, pagesSitemap }
